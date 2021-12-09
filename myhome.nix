1ece43111179ad752feec073845d3351b2e755a1
Nathaniel Griswold <nategriswold@gmail.com>
2021-12-09 02:07:34 -0600

    yarn stuff

--- myhome.nix
+++ myhome.nix
@@ -2,7 +2,7 @@
   overlay = final: prev: let
     pkgs = final.pkgs;
   in {
-    homedirPackages = with final; [ hugo emacs sieve-connect gnupg nodejs ripgrep github-cli ];
+    homedirPackages = with final; [ hugo emacs sieve-connect gnupg nodejs ripgrep github-cli yarn yarn2nix ];
     nodejs = final.nodejs-16_x;
 
     nwgHome = final.pkgs.symlinkJoin {
