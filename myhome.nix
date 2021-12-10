df06d2d97fbf598595561f84fc8c0a5094b82cad
Nathaniel Griswold <nategriswold@gmail.com>
2021-12-09 20:15:31 -0600

    switch back to yarn, add node settings

--- myhome.nix
+++ myhome.nix
@@ -2,10 +2,12 @@
   overlay = final: prev: let
     pkgs = final.pkgs;
   in {
-    homedirPackages = with final; [ hugo emacs sieve-connect gnupg nodejs ripgrep github-cli yarn yarn2nix ];
-    nodejs = final.nodejs-16_x;
+    homedirPackages = with final; [ hugo emacs sieve-connect gnupg nodejs_latest ripgrep github-cli jq yarn yarn2nix ];
+    nodejs = final.nodejs_latest;
+    nodejs-slim = final.nodejs-slim_latest;
+    nodePackages = final.nodePackages_latest;
 
-    nwgHome = final.pkgs.symlinkJoin {
+    nwgHome = pkgs.symlinkJoin {
       name = "nwg-home";
       paths = final.homedirPackages;
     };
