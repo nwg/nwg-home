d512ca9b1dc43475180063294f0bc55a3067416b
Nathaniel Griswold <nwg@nan.sh>
2022-01-03 12:42:40 -0600

    drop spam

--- my-home.nix
+++ my-home.nix
@@ -24,26 +24,6 @@
       nodejs = final.nodejs-14_x;
       nodejs-slim = final.nodejs-slim-14_x;
 
-      /*
-      vlc = let
-        withDarwin = self: with self; {
-          stdenv = prev.stdenv // {
-            mkDerivation = args: prev.stdenv.mkDerivation (args // {
-              meta.platforms = args.meta.platforms ++ final.lib.platforms.darwin;
-            });
-          };
-        };
-
-        darwinScope = pkgs.lib.makeScope prev.libsForQt5.newScope withDarwin;
-      in
-        darwinScope.callPackage "${nixpkgs}/pkgs/applications/video/vlc" {};
-        */
-      /*
-      vlc = prev.vlc.overrideAttrs (oldAttrs: {
-        meta.platforms = oldAttrs.meta.platforms ++ lib.platforms.darwin;
-      });
-      */
-
       myHome = pkgs.symlinkJoin {
         name = "myhome";
         paths = final.profilePackages;
